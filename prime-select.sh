73cb67d5b6246e760b4692246998e48ee532fa68
Bo Simonsen <bo@geekworld.dk>
2015-11-22 18:10:45 +0100

    Preperations to add rpm package

--- prime-select.sh
+++ prime-select.sh
@@ -6,15 +6,18 @@
 
 type=$1
 
-pwd=`dirname $(readlink -f "$0")`
-xorg_conf="$pwd/xorg.conf"
-offload="$pwd/prime-offload.sh"
+xorg_conf="/etc/prime/xorg.conf"
+offload="/etc/prime/prime-offload.sh"
+
 gpu_info=`nvidia-xconfig --query-gpu-info`
 nvidia_busid=`echo "$gpu_info" |grep -i "PCI BusID"|sed 's/PCI BusID ://'|sed 's/ //g'`
 
+libglx_nvidia=`update-alternatives --list libglx.so|grep nvidia-libglx.so`
+libglx_xorg=`update-alternatives --list libglx.so|grep xorg-libglx.so`
+
 case $type in
   nvidia)
-      update-alternatives --set libglx.so /usr/lib64/xorg/modules/extensions/nvidia/nvidia-libglx.so
+      update-alternatives --set libglx.so $libglx_nvidia
 
       ln -s $offload /etc/X11/xinit/xinitrc.d/prime-offload.sh
 
@@ -28,7 +31,8 @@ case $type in
       ldconfig
   ;;
   intel)
-      update-alternatives --set libglx.so /usr/lib64/xorg/modules/extensions/xorg/xorg-libglx.so
+      update-alternatives --set libglx.so $libglx_xorg
+
       rm -f /etc/X11/xinit/xinitrc.d/prime-offload.sh
       rm -f /etc/X11/xorg.conf.d/90-nvidia.conf
       rm -f /etc/ld.so.conf.d/nvidia-gfxG04.conf
